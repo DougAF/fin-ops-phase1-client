4bbe9475b753d55768bd883aa4fa78e1de081eaf
edkrueger <edkrueger@gmail.com>
2019-11-25 19:10:28 +0000

    Get the MAC address.

--- client/client.py
+++ client/client.py
@@ -1,5 +1,13 @@
+import re
+import uuid
 from bluepy.btle import Scanner
- 
+
+def get_mac():
+    mac = hex(uuid.getnode())
+    return ":".join([mac[i : i + 2] for i in range(2, len(mac), 2)]).replace("x", "0") 
+
+print(get_mac())
+
 scanner = Scanner()
 
 while True:
