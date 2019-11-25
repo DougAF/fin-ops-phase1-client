77ec5a284107d97f0ea9a440e38f94a46c9d514a
edkrueger <edkrueger@gmail.com>
2019-11-25 19:13:27 +0000

    Get BLE MAC too.

--- client/client.py
+++ client/client.py
@@ -1,12 +1,24 @@
+import subprocess
 import re
 import uuid
 from bluepy.btle import Scanner
 
 def get_mac():
     mac = hex(uuid.getnode())
-    return ":".join([mac[i : i + 2] for i in range(2, len(mac), 2)]).replace("x", "0") 
+    return ":".join([mac[i : i + 2] for i in range(2, len(mac), 2)]).replace("x", "0")
 
+def get_ble_mac():
+    cmd = "hcitool dev"
+    result = subprocess.run(["hcitool", "dev"], capture_output=True)
+    out = result.stdout
+    mac_regex = re.compile(r"[0-9a-f]{2}(:[0-9a-f]{2}){5}", re.IGNORECASE)
+    mac = mac_regex.search(out.decode("utf-8")).group()
+    return mac
+
+scanner = Scanner()
+ 
 print(get_mac())
+print(get_ble_mac())
 
 scanner = Scanner()
 
